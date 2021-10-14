f2a6ab96c4b59faaf3fa4607123dcefbbb6eabe2
Gurvinder Daheley <gurvinder.daheley@manifesto.co.uk>
2021-10-14 17:01:43 +0100

    Inital commit

---
+++ admin/partials/pr-bazaarvoice-default-textarea.php
@@ -0,0 +1,4 @@
+
+
+<textarea rows="10" cols="100" id="<?php echo $bazaarvoice_field_name; ?>" name="<?php echo PR_BAZAARVOICE_NAME; ?>[<?php echo $bazaarvoice_field_name; ?>]"><?php if(!empty($bazaarvoice_field_value)) { echo $bazaarvoice_field_value; } ?></textarea>
+
