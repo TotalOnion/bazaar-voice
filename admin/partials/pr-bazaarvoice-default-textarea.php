39f7715e5fa368911346d42372747e878b294191
Gurvinder Daheley <gurvinder.daheley@manifesto.co.uk>
2021-10-15 09:44:45 +0100

    Minor updates

--- admin/partials/pr-bazaarvoice-default-textarea.php
+++ admin/partials/pr-bazaarvoice-default-textarea.php
@@ -1,4 +1,2 @@
-
-
-<textarea rows="10" cols="100" id="<?php echo $bazaarvoice_field_name; ?>" name="<?php echo PR_BAZAARVOICE_NAME; ?>[<?php echo $bazaarvoice_field_name; ?>]"><?php if(!empty($bazaarvoice_field_value)) { echo $bazaarvoice_field_value; } ?></textarea>
+<textarea rows="10" cols="100" id="<?php echo $bazaarvoice_field_name; ?>" name="<?php echo PR_BAZAARVOICE_NAME; ?>[<?php echo $bazaarvoice_field_name; ?>]"><?php  echo $bazaarvoice_field_value; ?></textarea>
 
