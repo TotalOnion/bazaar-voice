d6fd0d36f3bb71dba9801227e5f93e76b16852a8
boodle <ben@totalonion.com>
2022-02-10 18:22:45 +0000

    Require PR Core.

--- includes/class-pr-bazaarvoice-activator.php
+++ includes/class-pr-bazaarvoice-activator.php
@@ -30,7 +30,21 @@ class Pr_Bazaarvoice_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+        // PR Core is required !
+        $pr_core_plugin = 'pr-core/pr-core.php';
+        if(
+            ! file_exists( trailingslashit( WPMU_PLUGIN_DIR ) . $pr_core_plugin)
+            && ! is_plugin_active( $pr_core_plugin )
+        ) {
+            wp_die(
+                'You must activate <strong>PR CORE plugin</strong> to activate this plugin. <br><a href="' . admin_url( 'plugins.php' ) . '">Return to Plugins page</a>'
+            );
+        }
+        
+        // Empty WP cache and Pantheon cache to reload headers
+        if ( function_exists( 'pantheon_clear_edge_all' ) ) {
+            pantheon_clear_edge_all();
+        }
+        wp_cache_flush();
 	}
-
 }
